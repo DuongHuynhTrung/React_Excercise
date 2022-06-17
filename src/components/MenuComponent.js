f352f7e51df516d9a8dfa3a7f79bbfb70a297db4
Huỳnh Trùng Dương <trungduong22021619@gmail.com>
2022-06-18 03:08:56 +0700

    Functional Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,75 +1,50 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
+// class Menu extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-        // onDishSelect(dish) {
-        //     this.setState({ selectedDish: dish })
-        // }
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-        // renderDish(dish) {
-        //     if (dish != null) {
-        //         return (
-        //             <Card>
-        //                 <CardImg top src={dish.image} alt={dish.name} />
-        //                 <CardBody>
-        //                     <CardTitle>{dish.name}</CardTitle>
-        //                     <CardText>{dish.description}</CardText>
-        //                 </CardBody>
-        //             </Card>
-        //         );
-        //     } else {
-        //         return (
-        //             <div></div>
-        //         );
-        //     }
-        // }
-
-    }
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                // Exercise 3.1
-                // <div key={dish.id} className="col-12 mt-5">
-                //     <Media tag="li">
-                //         <Media left middle>
-                //             <Media object src={dish.image} alt={dish.name} />
-                //         </Media>
-                //         <Media body className="ml-5">
-                //             <Media heading>{dish.name}</Media>
-                //             <p>{dish.description}</p>
-                //         </Media>
-                //     </Media>
-                // </div>
-                // Exercise 3.2
-                // <div className="col-12 col-md-5 m-1">
-                //     <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
-                //         <CardImg width="100%" src={dish.image} alt={dish.name} />
-                //         <CardImgOverlay>
-                //             <CardTitle>{dish.name}</CardTitle>
-                //         </CardImgOverlay>
-                //     </Card>
-                // </div>
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id} onClick={() => this.props.onClick(dish.id)} >
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+const Menu = (props) => {
+    // const menu = this.props.dishes.map((dish) => {
+    //     return (
+    //         <div className="col-12 col-md-5 m-1">
+    //             <Card key={dish.id} onClick={() => this.props.onClick(dish.id)} >
+    //                 <CardImg width="100%" src={dish.image} alt={dish.name} />
+    //                 <CardImgOverlay>
+    //                     <CardTitle>{dish.name}</CardTitle>
+    //                 </CardImgOverlay>
+    //             </Card>
+    //         </div>
+    //     );
+    // });
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
 }
+
 
 export default Menu;
